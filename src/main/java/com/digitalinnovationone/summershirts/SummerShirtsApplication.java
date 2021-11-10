cd6e036b3fe62a586cf1bf3b8f5f12f9a45119d5
Wagner Mello <wagnermello903@gmail.com>
2021-11-10 16:09:27 -0300

    Project created

---
+++ src/main/java/com/digitalinnovationone/summershirts/SummerShirtsApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.summershirts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerShirtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerShirtsApplication.class, args);
+	}
+
+}
