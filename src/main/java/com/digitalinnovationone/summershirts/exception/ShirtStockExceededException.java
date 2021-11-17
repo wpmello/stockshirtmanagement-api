980a8125801f983b72940d84c510867fc529c47f
Wagner Mello <wagnermello903@gmail.com>
2021-11-17 16:56:02 -0300

    Developed controller tests for increment operation

--- src/main/java/com/digitalinnovationone/summershirts/exception/ShirtStockExceededException.java
+++ src/main/java/com/digitalinnovationone/summershirts/exception/ShirtStockExceededException.java
@@ -4,8 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class ShirtStockExceededException extends Throwable {
+public class ShirtStockExceededException extends Exception {
     public ShirtStockExceededException(Long id, int quantityToIncrement) {
-        super(String.format("Shirt with ID %d informed to increment exceeds the max stock capacity: %d", id, quantityToIncrement));
+        super(String.format("Shirt with ID %d informed to increment exceeds the max stock capacity. " +
+                "Informed quantity: %d. Verify max quantity from product and try again!", id, quantityToIncrement));
     }
 }
